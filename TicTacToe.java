1af7e6c430c0229d51e0803f7203212ee70ca58b
Shravya <70620812+ShravyaK-Hub@users.noreply.github.com>
2020-10-03 12:44:57 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,9 +10,7 @@ public class TicTacToe {
 
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.creatingBoard();
-        ticTacToe.playerTurn();
-        ticTacToe.showBoard();
-        ticTacToe.selectIndex();
+        
     }
 
     //UC1 - Creating empty board
@@ -21,49 +19,6 @@ public class TicTacToe {
             board[indexValue] = " ";
         }
 
-    }
-
-    //UC2 - Choosing tokens
-    public void playerTurn() {
-        String playerToken, computerToken;
-        scanner = new Scanner(System.in);
-        System.out.println("Choose x or o: ");
-        if(scanner.nextLine().equals("x")) {
-            playerToken = scanner.nextLine();
-            computerToken = "o";
-        } else if (scanner.nextLine().equals("x")){
-            playerToken = scanner.nextLine();
-            computerToken = "x";
-        } else {
-            System.out.println("Invalid option");
-        }
-
-
-    }
-
-    //UC3 - Display board
-    public void showBoard() {
-            System.out.println(board[1] + "  |  " + board[2] + " |  " + board[3]);
-            System.out.println("-------------");
-            System.out.println(board[4] + "  |  " + board[5] + " |  " + board[6]);
-            System.out.println("-------------");
-            System.out.println(board[7] + "  |  " + board[8] + " |  " + board[9]);
-    }
-
-    //UC4 - Selecting index
-    public void selectIndex() {
-        scanner = new Scanner(System.in);
-        System.out.println("Choose index (1-9) to place token: ");
-        int index = scanner.nextInt();
-        if (index >= 1 && index <= 9) {
-            if(!board[index].equals("x") && !board[index].equals("o")) {
-                System.out.println("Token placed");
-            } else {
-                System.out.println("Index taken");
-            }
-        } else {
-            System.out.println("Invalid input! Enter input between 1 and 9");
-        }
     }
 }
 
